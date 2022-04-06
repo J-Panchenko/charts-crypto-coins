ddbbc94b631dabf95ce7b736daa33e825eee590b
j-panchenko <il586419001@gmail.com>
2022-04-06 14:59:17 +0300

    add basic setting

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../header';
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1 className="title">CryptoCoins LIVE</h1>
+    </header>
+  );
+}
+
+export default Header;
